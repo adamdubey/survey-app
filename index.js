7979de5633c8820c6ece420150ac83af0c717a18
Adam Dubey <adubey0311@gmail.com>
2020-08-07 08:50:57 -0500

    [AD][8M0720] addressing backend server refactoring tasks

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+
+// NOTE: The order of these matters; DO NOT CHANGE!!!
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-// default route handler
-app.get("/", (req, res) => {
-  res.send({ greetings: "citizen!" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 // dynamic port binding
 const PORT = process.env.PORT || 5000;
