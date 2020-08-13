571f2605f77e05ae47a9598e646ab9b635763199
Adam Dubey <adubey0311@gmail.com>
2020-08-13 12:06:58 -0500

    [AD][8M13207] Cleanup tasks and documentation updates

--- client/src/index.js
+++ client/src/index.js
@@ -6,13 +6,9 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
 
-// components
 import App from "./components/App";
-
-// reducers
 import reducers from "./reducers";
 
-// dummy reducer `() => []`
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDom.render(
@@ -21,6 +17,3 @@ ReactDom.render(
   </Provider>,
   document.querySelector("#root")
 );
-
-console.log("stripe key:", process.env.REACT_APP_STRIPE_KEY);
-console.log("env is:", process.env.NODE_ENV);
